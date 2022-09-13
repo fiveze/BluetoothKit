a517188d585cd3f0aa73de718af908523a06ff49
Sergey Tursunov <fiveze@gmail.com>
2022-09-13 13:01:03 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BluetoothKit",
-            path: "Source",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "BluetoothKitTests",
             dependencies: ["BluetoothKit"]),
